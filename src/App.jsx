caa0a55579198e2fc42bae5966ec85921c189cb6
sunnatullo54 <sultanovural28@gmail.com>
2025-03-20 16:30:00 +0500

    Rezalits

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>app</div>
+  );
+};
 
-export default App
+export default App;
