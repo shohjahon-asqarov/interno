5e87a665071d1397f8d4a7bbcda681232cc2f21d
kamola02007 <abduvaxobovakamola07@gmail.com>
2025-03-20 16:35:59 +0500

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Design from './components/Design'
+import About from './components/About'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+    <Design/>
+    <About/>
+    </>
   )
 }
 
