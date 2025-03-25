728d173eac85e9992c42bd7d2dfb04e17b431750
kamola02007 <abduvaxobovakamola07@gmail.com>
2025-03-25 16:16:06 +0500

    responsive add

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,25 +5,30 @@ import kitchen from '../assets/images/kitchen.png'
 const About = () => {
   return (
     <div>
-      <div className='container flex justify-between items-center py-20'>
-        <div className='space-y-10'>
+      <div className='container py-20'>
+        <div className='flex flex-col md:flex-row justify-between items-center space-y-10 '>
+          <div className='md:w-1/2 space-y-10'>
             <h1 className='text-4xl'>We Tackle The Most <br /> Challenging Designs</h1>
-            <p className='text-[#4D5053]'>The world needs innovators and problem solvers who <br /> turn challenges into greater opportunities. We have <br /> an insatiable curiosity about transformative trends <br /> challenging the status.</p>
+            <p className='text-[#4D5053]'>
+              The world needs innovators and problem solvers who <br /> turn challenges into greater opportunities. We have <br /> an insatiable curiosity about transformative trends <br /> challenging the status.
+            </p>
             <div className='flex items-center space-x-5'>
-                <img src={call} alt="" />
-                <p className='text-[#4D5053] text-lg'>012345678 <br />
-                Call Us Anytime</p>
-
+              <img src={call} alt="" />
+              <p className='text-[#4D5053] text-lg'>
+                012345678 <br />
+                Call Us Anytime
+              </p>
             </div>
             <button className='bg-[#292F36] text-white rounded-xl w-48 h-12'>Get Free Estimate</button>
-        </div>
-        
-        <div>
-            <img src={kitchen} alt="" />
+          </div>
+
+          <div className=' mt-10 '>
+            <img src={kitchen} alt="" className='w-full h-auto' />
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- src/components/Design.jsx
+++ src/components/Design.jsx
@@ -5,40 +5,46 @@ import right from '../assets/icons/arrow-right.svg'
 const Design = () => {
   return (
     <div>
-      <div className='container py-20 '>
-        <ul className='grid grid-cols-3 text-center '>
-            <li>
-                <h3 className='mb-6 text-lg'>Interior Design</h3>
-                <p className='text-[#4D5053]'>Enhancing the interior to achieve <br /> a healthier environment for the <br /> people using right space.</p>
-                <div className='flex justify-center'>
-                <button className='text-[#4D5053] flex gap-3 mt-10'>
-                View More 
+      <div className='container py-20'>
+        <ul className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 text-center'>
+          <li>
+            <h3 className='mb-6 text-lg'>Interior Design</h3>
+            <p className='text-[#4D5053]'>
+              Enhancing the interior to achieve <br /> a healthier environment for the <br /> people using right space.
+            </p>
+            <div className='flex justify-center'>
+              <button className='text-[#4D5053] flex gap-3 mt-10'>
+                View More
                 <img src={right} alt="" />
-                </button>
-                </div>
-            </li>
+              </button>
+            </div>
+          </li>
 
-            <li>
-                <h3 className='mb-6 text-lg'>Interior Design</h3>
-                <p className='text-[#4D5053]'>Enhancing the interior to achieve <br /> a healthier environment for the <br /> people using right space.</p>
-                <div className='flex justify-center'>
-                <button className='text-[#4D5053] flex gap-3 mt-10'>
-                View More 
+          <li>
+            <h3 className='mb-6 text-lg'>Interior Design</h3>
+            <p className='text-[#4D5053]'>
+              Enhancing the interior to achieve <br /> a healthier environment for the <br /> people using right space.
+            </p>
+            <div className='flex justify-center'>
+              <button className='text-[#4D5053] flex gap-3 mt-10'>
+                View More
                 <img src={right} alt="" />
-                </button>
-                </div>
-            </li>
+              </button>
+            </div>
+          </li>
 
-            <li>
-                <h3 className='mb-6 text-lg'>Interior Design</h3>
-                <p className='text-[#4D5053]'>Enhancing the interior to achieve <br /> a healthier environment for the <br /> people using right space.</p>
-                <div className='flex justify-center'>
-                <button className='text-[#4D5053] flex gap-3 mt-10'>
-                    View More 
+          <li>
+            <h3 className='mb-6 text-lg'>Interior Design</h3>
+            <p className='text-[#4D5053]'>
+              Enhancing the interior to achieve <br /> a healthier environment for the <br /> people using right space.
+            </p>
+            <div className='flex justify-center'>
+              <button className='text-[#4D5053] flex gap-3 mt-10'>
+                View More
                 <img src={right} alt="" />
-                </button>
-                </div>
-            </li>
+              </button>
+            </div>
+          </li>
         </ul>
       </div>
     </div>
